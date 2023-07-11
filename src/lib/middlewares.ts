7a680f16f6f3f1f7d7205479b42981da19783977
Matthieu Porte <matthieu@famille-porte.eu>
2023-07-11 10:18:04 +0200

    added utility-classes and middleware tests

--- src/lib/middlewares.ts
+++ src/lib/middlewares.ts
@@ -15,8 +15,7 @@ declare module "express-serve-static-core" {
 export const errorHandler = (
     error: Error,
     _: Request,
-    response: Response,
-    _next: NextFunction // eslint-disable-line no-unused-vars
+    response: Response
 ) => {
     response
         .status("statusCode" in error ? (error.statusCode as number) : 500)
